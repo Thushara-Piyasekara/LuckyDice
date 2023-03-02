6a3f12c2eda6f7a8d6238974b758c0ab98eacd59
Thushara-Piyasekara <btgp2001@gmail.com>
2023-03-03 02:30:04 +0530

    Polished the about popup. Added the new game button action listener.

---
+++ app/src/main/java/com/example/dicegame_cw1/GameScreen.kt
@@ -0,0 +1,13 @@
+package com.example.dicegame_cw1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_screen)
+
+
+    }
+}
--- app/src/main/java/com/example/dicegame_cw1/MainActivity.kt
+++ app/src/main/java/com/example/dicegame_cw1/MainActivity.kt
@@ -1,34 +1,51 @@
 package com.example.dicegame_cw1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.PopupWindow
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_menu)
 
+        //Setting actionListener for "About" button
+        val aboutButt = findViewById<Button>(R.id.aboutButt)
+        aboutButt.setOnClickListener {
+            showPopUpWindow(aboutButt)
+        }
+
+        //Setting actionListener for "New Game" button
+        val newGameButt = findViewById<Button>(R.id.newGameButt)
+        newGameButt.setOnClickListener {
+            val intent = Intent(this, GameScreen::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    //Makes the ABOUT popup window visible
+    @SuppressLint("InflateParams")
+    private fun showPopUpWindow(view: View) {
         val aboutPopup = layoutInflater.inflate(R.layout.about_popup_layout, null)
         val popupWindow = PopupWindow(
             aboutPopup,
             WindowManager.LayoutParams.MATCH_PARENT,
             WindowManager.LayoutParams.WRAP_CONTENT
         )
-
         val popupCloseButt = aboutPopup.findViewById<Button>(R.id.popupClose)
+        //Setting OnClickListener for the close button in the about popup
         popupCloseButt.setOnClickListener {
             popupWindow.dismiss()
         }
-
-        val aboutButt = findViewById<Button>(R.id.aboutButt)
-
-        aboutButt.setOnClickListener {
-            popupWindow.showAtLocation(aboutButt, Gravity.CENTER, 0, 0)
-        }
-
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
     }
+
+
 }
